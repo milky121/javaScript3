fc214a36585314a9a41638d35e64a74691d3cf02
milky121 <milkyrani8@gmail.com>
2024-03-29 16:17:02 +0530

    callback hell2

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ console.log("hello.......");
 
 // callback hell
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color , delay , nextColorChange) { 
-    setTimeout(()=>{
-        h1.style.color = "color";
-        if (nextColorChange) nextColorChange();
-    }, delay);
+// function changeColor(color , delay , nextColorChange) { 
+//     setTimeout(()=>{
+//         h1.style.color = "color";
+//         if (nextColorChange) nextColorChange();
+//     }, delay);
 
-}
+// }
 
-changeColor("red" ,1000, ()=>{
-    changeColor("orange",1000, () => {
-        changeColor("purple",1000,() => {
-           changeColor("green",1000);
-        });
-    });
-});
+// changeColor("red" ,1000, ()=>{
+//     changeColor("orange",1000, () => {
+//         changeColor("purple",1000,() => {
+//            changeColor("green",1000);
+//         });
+//     });
+// });
 // setTimeout(()=>{
 //     h1.style.color = "red";
 // },1000);
@@ -53,3 +53,32 @@ changeColor("red" ,1000, ()=>{
 // setTimeout(()=>{
 //     h1.style.color = "green";
 // },3000);
+
+// promises  
+
+
+function savetoDb(data , success, failure)  {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1 ;
+    if (internetSpeed > 4) {
+        success();
+    } else{
+        failure();
+    }
+}
+
+savetoDb("apna college" , () =>{
+    console.log(" success: your data was saved : ");
+    savetoDb(
+        "hello world",
+        () =>{
+            console.log("success2: data2 saved");
+        },
+        () =>{
+            console.log("failure2 : weak connection");
+        }
+    );
+},
+() => {
+    console.log("weak connection , data not saved");
+}
+);
