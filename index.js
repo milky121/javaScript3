c503c83c4446f515da9919aa623d964c70c9bb67
milky121 <milkyrani8@gmail.com>
2024-03-30 13:59:07 +0530

    text changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ console.log("hello.......");
 
 // }
 
-// changeColor("red" ,1000, ()=>{
+// changeColor("red" ,1000, ()=> {
 //     changeColor("orange",1000, () => {
 //         changeColor("purple",1000,() => {
 //            changeColor("green",1000);
@@ -83,6 +83,46 @@ changeColor("red" , 1000)
     console.log("yellow color was changed");
     
 });
+// sec = document.querySelector(".sec");
+// function changeColor(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             sec.style.color = color;
+//             resolve("color changed!!");
+//         }, delay);
+//     });
+// }
+
+// changeColor("red" , 1000)
+// .then(() => {
+//     console.log("red color was changed");
+//     return changeColor("orange" , 1000);
+// })
+// .then(() => {
+//     console.log("orange color was changed");
+//     return changeColor("purple", 1000);
+// })
+// .then(() => {
+//     console.log("purple color was changed");
+//     return changeColor("green", 1000);
+// })
+// .then(() => {
+//     console.log("green color was changed");
+//     return changeColor("pink", 1000);
+// })
+
+// .then(() => {
+//     console.log("pink color was changed");
+//     return changeColor("blue", 1000);
+// })
+// .then(() => {
+//     console.log("blue color was changed");
+//     return changeColor("yellow", 1000);
+// })
+// .then(() => {
+//     console.log("yellow color was changed");
+    
+// });
 
 
 
